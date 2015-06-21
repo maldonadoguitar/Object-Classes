d96117858267019be348b6b54c35c8c328f87278
Christian <maldonadoguitar@gmail.com>
2015-06-21 11:44:49 -0400

    commit already

--- CodeBank/CodeBank/main.m
+++ CodeBank/CodeBank/main.m
@@ -8,10 +8,60 @@
 
 #import <Foundation/Foundation.h>
 
+
+@interface BankAccount: NSObject
+
+- (void)balance:(CGFloat)bal;
+- (void)setWithdrawl:(CGFloat)wit;
+- (void)setDeposit:(CGFloat)dep;
+
+@end
+
+@implementation BankAccount{
+    
+    CGFloat balance;
+    CGFloat withdrawl;
+    CGFloat deposit;
+}
+
+- (void) printBalance {
+    NSLog(@"%f",balance);
+}
+- (void) printWithdrawl {
+    NSLog(@"%f-%f",balance,withdrawl);
+}
+- (void) printDeposit {
+    NSLog(@"%f-%f",balance,deposit);
+}
+    
+@end
+
+
+@interface Patron: NSObject
+
+- (void)name:(NSString *)person;
+- (void)accountNum:(NSInteger)acc;
+- (void)pocket:(NSInteger)poc;
+
+@end
+
+@implementation Patron{
+    
+    NSString
+    
+    
+}
+
+@end
+
+
+
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        
     }
     return 0;
 }
